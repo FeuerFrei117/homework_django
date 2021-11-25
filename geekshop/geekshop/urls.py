6c086da75f80c02c08b3f7ed32b399500bd7ddab
Roman <Chepkinrv@mail.ru>
2021-11-25 12:35:46 +0700

    Добавил домашнее задание 1

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp.views import index, contact, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('products/', products, name='products')
 ]
